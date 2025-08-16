21db0885ca96357fe40e1218b6cc553278df0311
saijaku1 <souta20101024@outlook.jp>
2025-08-16 20:09:26 +0900

    Create script.js

---
+++ farce/story3/script.js
@@ -0,0 +1,35 @@
+function checkVisibility() {
+  const elements = document.querySelectorAll(".talk");
+  const images = document.querySelectorAll(".show-image");
+
+  elements.forEach((element) => {
+    const rect = element.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {
+      element.classList.add("visible");
+    }
+  });
+
+  images.forEach((image) => {
+    const rect = image.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom >= 0) {
+      image.classList.add("visible");
+    } else {
+      image.classList.remove("visible");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkVisibility);
+document.addEventListener("DOMContentLoaded", checkVisibility);
+
+document.querySelector(".returnBtn").addEventListener("click", () => {
+  window.location.href = "https://saijaku.f5.si/farce/";
+});
+
+document.querySelector(".nextBtn").addEventListener("click", () => {
+  window.location.href = "https://saijaku.f5.si/farce/story4/";
+});
+document.querySelector(".backBtn").addEventListener("click", () => {
+  window.location.href = "https://saijaku.f5.si/farce/story2/";
+});
+  
